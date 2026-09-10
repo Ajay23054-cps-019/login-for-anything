a3cd45e64ac402cbdf62c776f877cb495d4b5184
ajay <ajayhemanth90@gmail.com>
2026-09-10 21:16:35 +0530

    Updated auth.py - added login and verify login functions

--- auth.py
+++ auth.py
@@ -19,11 +19,15 @@ class auth:
 
     def add_user(self,name,email,password):
         uid = str(uuid.uuid4())
-        if self.check_uid(uid):
-            self.cur.execute("INSERT INTO users (uid, name, email, password) VALUES (?,?,?,?)",(uid,name,email,self.hash_password(password)))
-            self.conn.commit()
-        else:
-            self.add_user(name,email,password)
+        try:
+            if self.check_uid(uid):
+                self.cur.execute("INSERT INTO users (uid, name, email, password) VALUES (?,?,?,?)",(uid,name,email,self.hash_password(password)))
+                self.conn.commit()
+                return uid
+            else:
+                self.add_user(name,email,password)
+        except:
+            return "User already exists or invalid details"
 
     def check_uid(self,uid):
         self.cur.execute("SELECT uid FROM users WHERE uid = ?",(uid,))
@@ -34,8 +38,21 @@ class auth:
     def hash_password(self,password):
         return hashlib.sha256(password.encode()).hexdigest()
 
-    def login(self):
-        ...
+    def login(self,uid,password):
+        self.cur.execute("SELECT password FROM users WHERE uid = ?",(uid,))
+        result = self.cur.fetchone()
+        if self.verify_login(password,result):
+            return "HI"
+        else:
+            return "Bye"
 
-    def verify_login(self):
-        ...
+
+    def verify_login(self,password,result):
+        if self.hash_password(password) == result[0]:
+            return True
+        else:
+            return False
+
+a = auth()
+print(a.add_user(name="Ajay",email="ajayhemanth90@gmail.com",password="Ajay@2008"))
+print(a.login("47e6b712-b02a-4921-aa5c-dda38397a5ce","Ajay@2008"))
